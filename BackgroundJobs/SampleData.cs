0952551167f604d04d3a31a95660e080b9c0008b
Emilio Garro Rangel <emiliogarrorangelxboxone@hotmail.com>
2025-03-15 22:48:38 -0600

    using concurrentBag to be able to use from different threads

---
+++ BackgroundJobs/SampleData.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Concurrent;
+
+namespace BackgroundJobs
+{
+    public class SampleData
+    {
+        public ConcurrentBag<string> Data { get; set; } = new ConcurrentBag<string>();
+    }
+}
