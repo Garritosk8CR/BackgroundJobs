d4d24eee56e79643293729ac47b97f865bcc72c1
Emilio Garro Rangel <emiliogarrorangelxboxone@hotmail.com>
2025-03-15 23:00:52 -0600

    Added singleton and started creating the background job

---
+++ BackgroundJobs/BackgroundRefresh.cs
@@ -0,0 +1,22 @@
+﻿
+namespace BackgroundJobs
+{
+    public class BackgroundRefresh : IHostedService, IDisposable
+    {
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- BackgroundJobs/Program.cs
+++ BackgroundJobs/Program.cs
@@ -1,8 +1,12 @@
+using BackgroundJobs;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
+
+builder.Services.AddSingleton<SampleData>();
 
 if (app.Environment.IsDevelopment())
 {
